74c5432e2ee9d1051c3377c27bcd9696dfeb3238
laspal <amit.laspal@gmail.com>
2019-03-12 12:00:04 +0530

    updates

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/amitlaspal/docker-example.git') {  node -> // is hudson.plugins.git.GitSCM
         }
     }
     triggers {
